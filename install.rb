3bcf226d2d255c3d0eb11e3d31ba6a55ec93ca20
Diego Carrion <dc.rec1@gmail.com>
2010-06-18 21:34:32 -0300

    dont override signal-auth

--- install.rb
+++ install.rb
@@ -1,10 +1,22 @@
 def copy(ext, dest = ".")
-  file = "signal-auth.#{ext}"
+  file = file_name(ext)
   src = File.join(File.dirname(__FILE__) , file)
-  dest = File.join(Rails.root, "config", dest, file)
+  dest = config_file(dest, file)
 
   FileUtils.cp src, dest
 end
 
-copy "yml"
+def config_file(folder, file)
+  File.join(Rails.root, "config", folder, file)
+end
+
+def file_name(ext)
+  "signal-auth.#{ext}"
+end
+
+def exists(ext)
+  File.exists? config_file(".", file_name(ext))
+end
+
+copy "yml" unless exists 'yml'
 copy "rb", "initializers"
