86dda68109e45ea41fe7f75157728f3d3a3bbe07
Diego Carrion <dc.rec1@gmail.com>
2010-06-21 15:22:06 -0300

    dont override config files on installation

--- install.rb
+++ install.rb
@@ -1,22 +1,10 @@
 def copy(ext, dest = ".")
-  file = file_name(ext)
+  file = "signal-auth.#{ext}"
   src = File.join(File.dirname(__FILE__) , file)
-  dest = config_file(dest, file)
+  dest = File.join(Rails.root, "config", dest, file)
 
   FileUtils.cp src, dest
 end
 
-def config_file(folder, file)
-  File.join(Rails.root, "config", folder, file)
-end
-
-def file_name(ext)
-  "signal-auth.#{ext}"
-end
-
-def exists(ext)
-  File.exists? config_file(".", file_name(ext))
-end
-
-copy "yml" unless exists 'yml'
+copy "yml"
 copy "rb", "initializers"
